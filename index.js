6fb3d1e262f65ddc09f27a28ec275e29250ac968
Rodrigo Melo <rmelo@Rodrigos-MacBook-Pro.local>
2017-05-29 20:04:57 -0300

    Add event module objects to index

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const { Command, CommandFactory, CommandDispatcher, CoreCommand, CommandBus } = require('./core/commands');
+const { Command, CommandFactory, CommandDispatcher, CommandBus } = require('./core/commands');
+const { Event, EventFactory, EventHandler, EventBus } = require('./core/events');
 
 module.exports = {
     Command,
     CommandFactory,
     CommandDispatcher,
     CoreCommand,
-    CommandBus
-}
+    CommandBus,
+    Event,
+    EventFactory,
+    EventHandler,
+    EventBus
+};
