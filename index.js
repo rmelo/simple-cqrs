ed0898b8db736a6b23fe5ed48a557ffc94f89bf5
Rodrigo Melo <rmelo@Rodrigos-MacBook-Pro.local>
2017-05-31 19:52:36 -0300

    Fix index file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const { Command, CommandFactory, CommandDispatcher, CommandBus } = require('./core/commands');
-const { Event, EventFactory, EventHandler, EventBus } = require('./core/events');
+const { Event, EventFactory, EventHandler, EventBus, EventDispatcher } = require('./core/events');
 
 module.exports = {
     Command,
     CommandFactory,
     CommandDispatcher,
-    CoreCommand,
     CommandBus,
     Event,
     EventFactory,
     EventHandler,
-    EventBus
+    EventBus,
+    EventDispatcher
 };
