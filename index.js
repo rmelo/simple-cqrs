dd463733f75672aa9793654e66b883805e90c85f
Rodrigo Melo <rmelo@Rodrigos-MacBook-Pro.local>
2017-06-05 15:23:11 -0300

    Add App facade by require('simple-cqrs').createApp()

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Command, CommandFactory, CommandDispatcher, CommandBus } = require('./co
 const { Event, EventFactory, EventHandler, EventBus, EventDispatcher } = require('./core/events')
 
 module.exports = {
+	createApp: () => {
+		return new App()
+	},
 	Command,
 	CommandFactory,
 	CommandDispatcher,
@@ -13,4 +16,29 @@ module.exports = {
 	EventHandler,
 	EventBus,
 	EventDispatcher
+}
+
+class App {
+	constructor() {
+		this._commandBus = new CommandDispatcher(new CommandFactory())
+		this._eventBus = new EventDispatcher()
+	}
+	useCommandBus(commandBus) {
+		this._commandBus = commandBus
+	}
+	useEventBus(eventBus) {
+		this._eventBus = eventBus
+	}
+	useEventHandler(handler) {
+		this._eventBus.register(handler)
+	}
+	useCommandHandler(type, handler) {
+		this._commandBus.register(type, handler)
+	}
+	send(command) {
+		this._commandBus.send(command)
+	}
+	publish(event) {
+		this._eventBus.publish(event)
+	}
 }
---
+++ index.spec.js
@@ -0,0 +1,63 @@
+/* global describe it*/
+'use strict'
+
+const
+	chai = require('chai'),
+	sinon = require('sinon'),
+	{ EventHandler, CommandDispatcher, EventDispatcher, CommandFactory } = require('./index.js')
+
+chai.should()
+chai.use(require('sinon-chai'))
+
+const app = require('./index.js').createApp()
+
+chai.should()
+
+describe('App tests', () => {
+
+	const handler = { handle: () => { } }
+	const spyHandler = sinon.spy(handler, 'handle')
+
+	it('Should register a command handler', (done) => {
+		app.useCommandHandler('createTrackingCommand', handler.handle)
+		done()
+	})
+
+	it('Should send a command and handle it', (done) => {
+		app.send({ type: 'createTrackingCommand' })
+		spyHandler.should.been.calledOnce
+		done()
+	})
+
+	const eventHandler = new class MyEventHandler extends EventHandler {
+		constructor() {
+			super()
+			this.register('myEvent', handler)
+		}
+		handle() { }
+	}
+	const spyEventHandler = sinon.spy(eventHandler, 'handle')
+
+	it('Should register an event handler', (done) => {
+		app.useEventHandler(eventHandler)
+		done()
+	})
+
+	it('Should send an event and handle it', (done) => {
+		app.publish({ type: 'myEvent' })
+		spyEventHandler.should.been.calledOnce
+		done()
+	})
+
+	it('Should change the command bus', (done) => {
+		app.useCommandBus(new CommandDispatcher(new CommandFactory()))
+		done()
+	})
+
+	it('Should change the event bus', (done) => {
+		app.useCommandBus(new EventDispatcher())
+		done()
+	})
+
+})
+
